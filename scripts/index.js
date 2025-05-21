c9fec0e65a8dae831c734f4e4f96aab7052f6070
FloraCom <flora.company07@gmail.com>
2025-05-21 14:11:14 +0530

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,62 @@ document.getElementById('menu').addEventListener('click', ()=>{
 document.getElementById('close').addEventListener('click', ()=>{
 	document.getElementById('asideMenu').classList.toggle('asideOpen');
 });
+
+function showData(products){
+
+	let content = document.getElementById('productsList');
+
+	content.innerHTML = '';
+	Object.values(Object.values(products)[0]).forEach((obj, index) => {
+
+		if (index < 10) {
+			const li = document.createElement('li');
+			li.className = 'productCard';
+			li.innerHTML = `
+				<a href="product.html?category=${obj.parentCategory}&sub=${obj.subCategory}&id=${obj.id}" class="cardA">
+					<div class="card">
+						<div class="cardImg">
+							<img class="cardImageView" src="${obj.variations[0].image}" onerror="this.onerror=null;this.src='./media/fc.png';">
+						</div>
+						<div class="cardText">
+							<p class="category">${obj.parentCategory}</p>
+							<h3 class="title">${obj.name}</h3>
+							<span class="priceSpan">499.00</span>
+							<h3 class="price">${obj.variations[0].price}</h3>
+						</div>
+					</div>
+					<button>Add To Cart</button>
+				</a>
+			`;
+			content.appendChild(li);
+		}
+	});
+
+		/*
+		<p><strong>ID:</strong> ${prod.id}</p>
+		<p><strong>Name:</strong> ${prod.name}</p>
+		<p><strong>Description:</strong> ${prod.description}</p>
+		<p><strong>Category:</strong> ${prod.parentCategory} > ${prod.subCategory}</p>
+		<p><strong>Tags:</strong> ${prod.tags}</p>
+		<p><strong>Stock Status:</strong> ${prod.stockStatus ? 'Active' : 'Inactive'}</p>
+		<p><strong>Variations:</strong></p>
+		<ul>
+		  ${prod.variations.map(v => `<li>${v.id} - ${v.name} - <a href="${v.image}" target="_blank">Image</a></li>`).join('')}
+		</ul>
+		*/
+}
+
+function display() {
+  fetch("./assets/products.json")
+  	.then((res) => {if (!res.ok) {} return res.json();})
+  	.then((data) => {
+	  
+	  let plants = Object.values(data)[0]['all']['plant'];
+
+	  showData(plants);
+  
+	}).catch();
+}
+
+
+display();
--- scripts/product.js
+++ scripts/product.js
@@ -7,3 +7,103 @@ document.getElementById('close').addEventListener('click', ()=>{
 });
 
 
+const params = new URLSearchParams(window.location.search);
+const category = params.get('category');
+const sub = params.get('sub');
+const id = params.get('id');
+
+let variation = 0;
+let product = '';
+let quantity = 0;
+
+function decrease(){
+	if (quantity > 0){
+		--quantity;
+	}
+	document.getElementById('quantity').innerHTML = quantity;
+	if (quantity >= 50) {
+		document.getElementById('quantity').style.color="green";
+		document.getElementById('quantity').style.fontSize="1.2rem";
+	} else {
+		document.getElementById('quantity').style.color="#000";
+		document.getElementById('quantity').style.fontSize="1rem";
+	}
+}
+
+function increase(){
+	++quantity;
+	document.getElementById('quantity').innerHTML = quantity;
+	if (quantity >= 50) {
+		document.getElementById('quantity').style.color="green";
+		document.getElementById('quantity').style.fontSize="1.2rem";
+	} else {
+		document.getElementById('quantity').style.color="#333333";
+		document.getElementById('quantity').style.fontSize="1rem";
+	}
+}
+
+function noProduct(){
+	document.getElementById('container').style.display = 'none';
+	document.getElementById('error').style.display = 'flex';
+}
+
+function changeVariation(variant){
+	variation = variant;
+	showData(product);
+}
+
+function populateContent(product) {
+
+	document.getElementById("prdImg").src = product.variations[variation].image;
+	document.getElementById("title").innerHTML = product.name;
+	document.getElementById("prdPrice").innerHTML = product.variations[variation].price;
+	document.getElementById("discountPrice").innerHTML = parseInt(parseInt(product.variations[variation].price)+(parseInt(product.variations[variation].price)*0.1));
+	document.getElementById("availability").innerHTML = (product.variations[variation].stock) ? 'Available In Stock' : 'Out Of Stock';
+	document.getElementById("description").innerHTML = (product.description);
+	let variations = document.getElementById("variations");
+	variations.innerHTML = '';
+	(product.variations).forEach((vari, index) => {
+		const label = document.createElement('label');
+
+		if (index == variation) {
+			label.innerHTML = `
+				<input type="radio" name="size-radio" checked="true" />
+				<p>${vari['name']}</p>
+			`;
+		} else {
+			label.innerHTML = `
+				<input type="radio" name="size-radio" onclick="changeVariation(${index});"/>
+				<p>${vari['name']}</p>
+			`;
+		}
+		variations.appendChild(label);
+	});
+}
+
+function showData(product){
+
+	if (product) {
+		populateContent(product);
+	} else {
+		noProduct();
+	}
+}
+
+function display() {
+  fetch("./assets/products.json")
+  	.then((res) => {if (!res.ok) {} return res.json();})
+  	.then((data) => {
+
+  		if (category && sub && id) {
+  			product = Object.values(data)[0]['all'][category][sub][id];
+	  		showData(product);
+  		} else {
+  			noProduct();
+  		}
+	  
+
+  
+	}).catch();
+}
+
+display();
